8ac55ee94fbd02ac76f5dcc2e6e46dc132ea7b51
Cristina <cristina@forecast.it>
2021-05-29 12:15:22 +0200

    forgot to save

--- node-nosql/controllers/projects.js
+++ node-nosql/controllers/projects.js
@@ -19,14 +19,8 @@ const filterReqBody = (reqBody) => {
 };
 
 exports.getProjects = (req, res, next) => {
-<<<<<<< HEAD
-  req.session.person.getCompany().then((company) => console.log(company));
-  Projects.findAll({
-    where: { company_id: req.person.company_id },
-=======
   Project.find({
     company_id: req.session.person.company_id,
->>>>>>> master
   })
     .then((projects) => {
       res.send({ response: projects });
